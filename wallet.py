c9e87a7f26f823ed3ca1646ff4116f02ab155e63
Bishwajit Jha <ajitjha393@gmail.com>
2020-10-20 20:18:51 +0530

    Generating Wallet keys

--- wallet.py
+++ wallet.py
@@ -1,6 +1,21 @@
+from binascii import hexlify
+from Crypto.PublicKey import RSA
+import Crypto.Random
+import binascii
+
+
 class Wallet:
     def __init__(self):
-        pass
+        self.private_key , self.public_key =  self.generate_keys()
+
 
     def generate_keys(self):
-        pass
+        private_key = RSA.generate(1024, Crypto.Random.new().read)
+        public_key = private_key.publicKey()
+
+        return (
+            binascii.hexlify(private_key.exportKey(format='DER')).decode('ascii'),
+            binascii.hexlify(public_key.exportKey(format='DER')).decode('ascii')
+        )
+
+print(Wallet().private_key)
