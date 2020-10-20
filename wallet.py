3eec6c78b118b60d895bba2f76c9c40ce9c5a329
Bishwajit Jha <ajitjha393@gmail.com>
2020-10-20 20:37:51 +0530

    Added Load and create RSA Keys in wallet

--- wallet.py
+++ wallet.py
@@ -6,8 +6,13 @@ import binascii
 
 class Wallet:
     def __init__(self):
+        self.private_key , self.public_key =  None , None
+
+    def create_keys(self):
         self.private_key , self.public_key =  self.generate_keys()
 
+    def load_keys(self):
+        pass
 
     def generate_keys(self):
         private_key = RSA.generate(1024, Crypto.Random.new().read)
