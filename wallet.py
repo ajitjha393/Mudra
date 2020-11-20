8b0e3be450cb42a4df3b0f5771a6faf662cc1b46
Bishwajit Jha <ajitjha393@gmail.com>
2020-11-20 07:27:22 +0530

    Saving and loading wallet keys

--- wallet.py
+++ wallet.py
@@ -1,5 +1,5 @@
 from binascii import hexlify
-from Crypto.PublicKey import RSA
+from Crypto.PublicKey import PublicKey, RSA
 import Crypto.Random
 import binascii
 
@@ -9,10 +9,22 @@ class Wallet:
         self.private_key , self.public_key =  None , None
 
     def create_keys(self):
-        self.private_key , self.public_key =  self.generate_keys()
+        private_key , public_key = self.generate_keys()
+        self.private_key , self.public_key =  private_key , public_key
+        with open('wallet.txt',mode='w') as f:
+            f.write(public_key)
+            f.write('\n')
+            f.write(private_key)
+            
+
 
     def load_keys(self):
-        pass
+        with open('wallet.txt', mode='r') as f:
+            keys = f.readlines()
+            public_key = keys[0][:-1]
+            private_key = keys[1]
+            self.public_key , self.private_key = public_key , private_key
+
 
     def generate_keys(self):
         private_key = RSA.generate(1024, Crypto.Random.new().read)
