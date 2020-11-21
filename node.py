a8f4f344878140531a7914fb74aa211e63f00137
Bishwajit Jha <ajitjha393@gmail.com>
2020-11-21 09:33:02 +0530

    Added dummy route decorators

--- node.py
+++ node.py
@@ -8,6 +8,10 @@ wallet = Wallet()
 
 CORS(app)
 
+@app.route('/',methods=['GET'])
+def get_ui():
+    return 'Hey, This server works!'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
 
