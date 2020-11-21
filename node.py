17e399adfbe127be7b71e37b024cedc7917a0e57
Bishwajit Jha <ajitjha393@gmail.com>
2020-11-21 09:17:28 +0530

    Setting up flask server and CORS setup

---
+++ node.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+from wallet import Wallet
+
+
+app = Flask(__name__)
+wallet = Wallet()
+
+CORS(app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
+
