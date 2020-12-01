02b4173292e807bbc905a1040a2c58f73f5bde6b
Bishwajit Jha <ajitjha393@gmail.com>
2020-12-01 16:40:01 +0530

    fn for Sending broadcasting req to peer nodes

--- blockchain.py
+++ blockchain.py
@@ -4,6 +4,7 @@ from functools import reduce
 from collections import OrderedDict
 import hashlib
 import json
+import requests
 
 from utility.hash_util import hash_block
 from utility.verification import Verification
@@ -186,6 +187,22 @@ class Blockchain:
         if Verification.verify_transaction(new_transaction, self.get_balance):
             self.__open_transactions.append(new_transaction)
             self.save_data()
+            for node in self.__peer_nodes:
+                url = 'http://{}/broadcast-transaction'.format(node)
+                try:
+                    response = requests.post(url,json={
+                            'sender': sender,
+                            'recipient': recipient,
+                            'amount': amount,
+                            'signature': signature
+                        })
+                    
+                    if response.status_code == 400 or response.status_code == 500:
+                        print('Transaction Declined, needs Resolving!')
+                        return False
+                except requests.exceptions.ConnectionError:
+                    continue        
+
             return True
         return False
 
