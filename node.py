8ed447da77a60082eb3d4ea0ba42892aff966033
Bishwajit Jha <ajitjha393@gmail.com>
2020-12-01 15:34:11 +0530

    Added ArgParser for getting port from cli

--- node.py
+++ node.py
@@ -238,5 +238,10 @@ def get_nodes():
     return jsonify(response), 200
 
 if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p','--port',default=5000)
+    args = parser.parse_args()
+    port = args.port
     app.run(host='0.0.0.0',port=5000)
 
