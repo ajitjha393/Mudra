febc8c432b305e8111275a430fbbcc0eebdb1ec5
Bishwajit Jha <ajitjha393@gmail.com>
2020-12-01 15:37:06 +0530

    Running multiple node server on -p ports

--- node.py
+++ node.py
@@ -240,8 +240,8 @@ def get_nodes():
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument('-p','--port',default=5000)
+    parser.add_argument('-p','--port',type=int ,default=5000)
     args = parser.parse_args()
     port = args.port
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=port)
 
